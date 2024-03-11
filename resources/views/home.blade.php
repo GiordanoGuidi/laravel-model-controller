161a9d73f1b9162d25ffcd9b86b08cf79ceadd6f
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-11 18:38:32 +0100

    added partial movies_card.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,7 @@
     <div class="row g-5">
         @foreach ($movies as $movie )            
         <div class="col-3">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">{{$movie->title}}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
-                    <p>{{$movie->nationality}}</p>
-                    <p>{{$movie->date}}</p>
-                    <p>{{$movie->vote}}</p>
-                </div>
-            </div>    
+           @include('includes.movies_card')
         </div>
         @endforeach
     </div>
