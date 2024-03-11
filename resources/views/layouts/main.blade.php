3d93c9d7a60204d84cc34fc4fc045cd8857640d8
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-11 16:29:30 +0100

    added file main.blade.php

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{Vite::asset('resources/img/hd.jpg')}}">
+    <title>{{env('APP_NAME', 'Movies')}} | @yield('title')</title>
+    {{--CDNS--}}
+    @yield('cdns')
+</head>
+<body>
+
+    {{--Main Content--}}
+    @yield('main-content')
+    
+</body>
+{{--Scripts--}}
+@yield('scipts')
+</html>
